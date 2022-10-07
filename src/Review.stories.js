dac6e0657c3558ed7d537631461737e775e20151
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2022-10-07 14:07:30 +0300

    review

---
+++ src/Review.stories.js
@@ -0,0 +1,55 @@
+import { DocumentEditor } from "@onlyoffice/document-editor-vue";
+import Review  from "./components/Review/Review.vue"
+import config from "../config/config.json";
+
+export default {
+  title: 'Samples/Work with review',
+  component: Review,
+  decorators: [() => ({ template: '<div style="height: 400px;"><story/></div>' })],
+  parameters: {
+    docs: false,
+  }
+};
+
+const Template = (args) => ({
+  components: { Review, DocumentEditor },
+  data() {
+    return {
+      documentServerUrl: args.documentServerUrl,
+      editorId: args.editorId,
+      config: {
+        document: {
+          fileType: "docx",
+          title: "demo.docx",
+          url: config.demoStorage + "demo.docx",
+          permissions: {
+            edit: false,
+            review: true
+          }
+        },
+        editorConfig: {
+          mode: "edit"
+        },
+        documentType: "word",
+        height: "600px"
+      },
+      connector: null
+    };
+  },
+  methods: {
+    onDocumentReady() {
+      var editor = window.DocEditor.instances[this.editorId];
+      var connector = editor.createConnector();
+      
+      this.connector = connector;
+    }
+  },
+  template: '<Review :connector="connector"  /> <DocumentEditor :id="editorId" :config="config" :documentServerUrl="documentServerUrl" :events_onDocumentReady="onDocumentReady" />',
+});
+
+export const CommentsTemplate = Template.bind({});
+CommentsTemplate.storyName = "Work with review";
+CommentsTemplate.args = {
+  editorId: "docxForReview",
+  documentServerUrl: config.documentServerUrl
+};
---
+++ src/components/Review/Review.vue
@@ -0,0 +1,58 @@
+<template>
+  <div className="reviewBlock">
+    <button v-on:click="prevReview">Prev</button>
+    <button v-on:click="nextReview">Next</button>
+    <button v-on:click="acceptReview">Accept</button>
+    <button v-on:click="rejectReview">Reject</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Review',
+  props: ['connector'],
+  methods: {
+    prevReview() {
+      this.connector.executeMethod("MoveToNextReviewChange", [false]);
+    },
+    nextReview() {
+      this.connector.executeMethod("MoveToNextReviewChange");
+    },
+    acceptReview() {
+      this.connector.executeMethod("AcceptReviewChanges");
+    },
+    rejectReview() {
+      this.connector.executeMethod("RejectReviewChanges");
+    }
+  }
+});
+</script>
+
+<style>
+  .reviewBlock {
+    text-align: center;
+    margin-bottom: 16px;
+  }
+
+  button {
+    font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-weight: 700;
+    border: 0;
+    border-radius: 3em;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 1;
+    font-size: 12px;
+    padding: 10px 16px;
+    color: #333;
+    background-color: transparent;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+    margin-right: 5px;
+  }
+
+  button:hover {
+    background: rgb(221, 235, 250);
+  }
+</style>
